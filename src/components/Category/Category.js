e72dd86108e911c8c4fbae6af68fa3239dc2be98
Elena <lenhenlady@gmail.com>
2020-12-23 20:18:40 +0300

    categories -> subcategories pages

--- src/components/Category/Category.js
+++ src/components/Category/Category.js
@@ -1,15 +1,23 @@
 import React from "react";
 import Poem from "../Poem/Poem";
+import { Link } from 'react-router-dom';
+import './Category.css';
 
 function Category(props) {
-    const poems = props.poems;
-    const poem = poems.map(item => <Poem key={item.id} poem = {item.poem} />);
+
+    const poems = props.subcategories.map(item => <Poem key={item.id} name={item.subname} subpoems={item.poem} />);
 
     return (
-        <div>
-            <h2 className="section__titile">{props.name}</h2>
-            {poem}
-        </div>
+        <Link className="category"
+            to={{ 
+                pathname: "/subcategory", 
+                data: poems, 
+                title: props.name 
+            }}
+        >
+            {props.name}
+        </Link>
+
     )
 }
 
---
+++ src/components/Subcategory/Subcategory.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+import Header from '../Header/Header';
+import './Subcategory.css';
+
+
+function Subcategory(props) {
+    
+    const subcat = props.location.data.map((item) => {
+        return <Link
+            className="subcategories__item"
+            to={{
+                pathname: "/complain",
+                data: item.props.subpoems,
+                title: item.props.name
+            }}
+        >
+           <p className="sub__name"> {item.props.name}</p>
+        </Link>
+    });
+
+    return (
+        <div className="section">
+            <Header title={`${props.location.title}`} />
+            <div className="subcategories">
+                {subcat}
+            </div>
+        </div>
+    )
+}
+
+export default Subcategory;
