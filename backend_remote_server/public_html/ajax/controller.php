2711c50b9b504acfddb981cdf4eeb650e516aa1d
Elena <lenhenlady@gmail.com>
2020-12-27 20:21:40 +0300

    Updates

--- backend_remote_server/public_html/ajax/controller.php
+++
@@ -1,6 +0,0 @@
-<?php
-define("CONFIG_DATABASE_PATH",$_SERVER["DOCUMENT_ROOT"]."/../database/db.sqlite");
-require("../../src/RequestHandler.php");
-
-$application = new RequestHandler();
-$application->RenderResponse();
